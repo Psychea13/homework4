b1701393086ba83ec4c416d09d29fd3bac423ac2
Psychea13 <kea260209@gmail.com>
2024-07-16 17:54:12 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+#Организация программ
+my_string = input('Введите что-нибудь, чтобы проверить это: ')
+print('Это то, что Вы только что ввели:', my_string)
+print('Количество символов введённого текста: ', len(my_string))
+
+#Методы строк
+print(my_string .upper())
+print(my_string .upper().lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
